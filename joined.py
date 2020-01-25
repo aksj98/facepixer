7b4e4c224f954bf7315db934443e06f5297e8959
aksj98 <akshaj.jain@gmail.com>
2020-01-26 01:49:52 +0530

    ppt added

--- joined.py
+++ joined.py
@@ -5,7 +5,7 @@ from PIL import Image
         s='0'+str(i)
     else:
         s=str(i)'''
-images = [Image.open(x) for x in ['./AR_cropped_sketches/Mp-002-1-sz1.jpg', './AR_cropped_sketches/Mp-003-1-sz1.jpg']]
+images = [Image.open(x) for x in ['images.jpg', 'images.jpg']]
 widths, heights = zip(*(i.size for i in images))
 
 total_width = sum(widths)
@@ -18,4 +18,4 @@ for im in images:
   new_im.paste(im, (x_offset,0))
   x_offset += im.size[0]
 
-new_im.save('mfmm1'+'.jpg')
+new_im.save('mfm'+'.jpg')
