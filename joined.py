36374ea1da94a7949b85e96e503b886365c71cc0
aksj98 <akshaj.jain@gmail.com>
2020-01-26 15:27:38 +0530

    screenshots

--- joined.py
+++ joined.py
@@ -5,7 +5,7 @@ from PIL import Image
         s='0'+str(i)
     else:
         s=str(i)'''
-images = [Image.open(x) for x in ['images.jpg', 'images.jpg']]
+images = [Image.open(x) for x in ['a.jpeg', 'a.jpeg']]
 widths, heights = zip(*(i.size for i in images))
 
 total_width = sum(widths)
@@ -18,4 +18,4 @@ for im in images:
   new_im.paste(im, (x_offset,0))
   x_offset += im.size[0]
 
-new_im.save('mfm'+'.jpg')
+new_im.save('aks'+'.jpg')
